42284dfbd99b5c91200514b5ad87954e70b65a1d
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-26 03:49:29 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,15 +32,17 @@
 
 def naming_system
   kingdom = {
-      :label => "Phylum" 
+      :label => "Phylum", 
       :sub_category => {
-          :label => "Order"
+        :label => "Class",
+        :sub_category => {
+          :label => "Order",
           :sub_category => {
-              :label => "Family"
+              :label => "Family",
               :sub_category => {
-                  :label => "Genus"
+                  :label => "Genus",
                   :sub_category => {
-                        :label => "Species"
+                        :label => "Species",
                         :sub_category => {}
             }
           }
